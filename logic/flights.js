97674b71583132acaad830ac94ec75c763fedfd0
TomHomer <46781470+TomHomer@users.noreply.github.com>
2020-05-05 10:02:42 +0100

    fdg

--- logic/flights.js
+++ logic/flights.js
@@ -4,6 +4,14 @@ function Flights() {
 
         let numFlights = 0;
 
+        if( numPassengers < 0 || !Number.isInteger(Number(numPassengers)) ) {
+            throw new Error("The number of passengers must be a positive integer value")
+        }
+
+        if( flightCap < 0 || !Number.isInteger(Number(flightCap)) ) {
+            throw new Error("The capacity of the flight must be a positive integer value")
+        }
+
         if(numPassengers % flightCap == 0) {
             numFlights = numPassengers / flightCap;
         }
@@ -14,7 +22,29 @@ function Flights() {
         return numFlights;
     }
 
-    return {calculateNumberOfFlights};
+    function checkAircraftRevision(distanceLimit, distances) {
+
+        let totalDistance = 0;
+        let returnString = '';
+
+        distances.forEach(distance => {
+            totalDistance += distance;
+        });
+
+        if(totalDistance <= distanceLimit/2) {
+            returnString = "The revision needs to be done within the next 3 months";
+        } else if(totalDistance <= distanceLimit*3/4) {
+            returnString = "The revision needs to be done within the next 2 months";
+        } else if(totalDistance <= distanceLimit) {
+            returnString = "The revision needs to be done within the next month";
+        } else if(totalDistance > distanceLimit) {
+            throw new Error("Total distance is over the distance limit");
+        }
+
+        return returnString;
+    }
+
+    return {calculateNumberOfFlights, checkAircraftRevision};
 }
 
 module.exports = Flights();
--- logic/frontend.js
+++ logic/frontend.js
@@ -12,7 +12,9 @@ function onCalculateNumberOfFlights() {
           " flight(s) to carry " + passengers + " passengers";
     } catch (error) {
         document.getElementById('flights').innerHTML = error; 
-    } 
+    } finally {
+        console.log("Entered " + passengers + " on " + capacity + "flights")
+    }
 }
 
 function onCalculateTotalFinalPrice(object) {
