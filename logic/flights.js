cc7f116a2bfd9e3e10229675c89edd2ec23c664b
TomHomer <46781470+TomHomer@users.noreply.github.com>
2020-05-05 08:52:39 +0100

    efs

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,21 @@
 function Flights() {
 
+    function calculateNumberOfFlights(numPassengers, flightCap) {
+
+        let numFlights = 0;
+
+        if(numPassengers % flightCap == 0) {
+            numFlights = numPassengers / flightCap;
+        }
+        else {
+            numFlights = Math.floor(numPassengers / flightCap) + 1;
+        }
+
+        return numFlights;
+    }
+
+    return {calculateNumberOfFlights};
 }
+
+module.exports = Flights();
 
